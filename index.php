8cc0f6a922963b49e9fa8da9d1cdf7fa5937aaf0
pruna_catalin <prunacatalin.costin@gmail.com>
2017-10-04 10:19:33 +0300

    =================V.0.0.1===================== -First commit from phpStorm -Create default file

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+/**
+ * Created by Pruna Catalin.
+ * User: Catalin
+ * Date: 10/4/2017
+ * Time: 10:15 AM
+ */
+
+$startAplication = date("d-m-y",time()); // date start
+$endAplication = date("d-m-y",time()); // date end
+
+?>
